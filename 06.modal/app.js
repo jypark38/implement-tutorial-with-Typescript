6987b69313a2a854230d5fa63278955ef1abeb34
jypark38 <jury124@naver.com>
2024-01-25 19:24:26 +0900

    refactor : type 추가 및 type guard

--- 06.modal/app.js
+++ 06.modal/app.js
@@ -2,30 +2,33 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+// query
 const modalBtn = document.querySelector(".modal-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
-modalBtn.addEventListener("click", () => {
-    modalOverlay.classList.add("open-modal");
-});
-closeBtn.addEventListener("click", () => {
-    modalOverlay.classList.remove("open-modal");
-});
-// ! 추가기능
-// 배경 클릭시 모달 닫기
-modalOverlay.addEventListener("click", () => {
-    modalOverlay.classList.remove("open-modal");
-});
-// 닫기 버튼에서 포커스 벗어나는 것 방지
-closeBtn.addEventListener("keydown", (e) => {
-    if (e.key === "Tab") {
-        e.preventDefault();
-    }
-});
-// esc 입력시 모달 닫기
-document.body.addEventListener("keydown", (e) => {
-    if (modalOverlay.classList.contains("open-modal") && e.key === "Escape") {
+if (modalBtn && modalOverlay && closeBtn) {
+    modalBtn.addEventListener("click", () => {
+        modalOverlay.classList.add("open-modal");
+    });
+    closeBtn.addEventListener("click", () => {
         modalOverlay.classList.remove("open-modal");
-    }
-});
+    });
+    // ! 추가기능
+    // 배경 클릭시 모달 닫기
+    modalOverlay.addEventListener("click", () => {
+        modalOverlay.classList.remove("open-modal");
+    });
+    // 닫기 버튼에서 포커스 벗어나는 것 방지
+    closeBtn.addEventListener("keydown", (e) => {
+        if (e.key === "Tab") {
+            e.preventDefault();
+        }
+    });
+    // esc 입력시 모달 닫기
+    document.body.addEventListener("keydown", (e) => {
+        if (modalOverlay.classList.contains("open-modal") && e.key === "Escape") {
+            modalOverlay.classList.remove("open-modal");
+        }
+    });
+}
 export {};
--- 06.modal/app.ts
+++ 06.modal/app.ts
@@ -3,31 +3,35 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
-const modalBtn = document.querySelector(".modal-btn");
-const modalOverlay = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
+// query
 
-modalBtn.addEventListener("click", () => {
-  modalOverlay.classList.add("open-modal");
-});
-closeBtn.addEventListener("click", () => {
-  modalOverlay.classList.remove("open-modal");
-});
+const modalBtn: HTMLButtonElement | null = document.querySelector(".modal-btn");
+const modalOverlay: HTMLDivElement | null = document.querySelector(".modal-overlay");
+const closeBtn: HTMLButtonElement | null = document.querySelector(".close-btn");
 
-// ! 추가기능
-// 배경 클릭시 모달 닫기
-modalOverlay.addEventListener("click", () => {
-  modalOverlay.classList.remove("open-modal");
-});
-// 닫기 버튼에서 포커스 벗어나는 것 방지
-closeBtn.addEventListener("keydown", (e: KeyboardEvent) => {
-  if (e.key === "Tab") {
-    e.preventDefault();
-  }
-});
-// esc 입력시 모달 닫기
-document.body.addEventListener("keydown", (e: KeyboardEvent) => {
-  if (modalOverlay.classList.contains("open-modal") && e.key === "Escape") {
+if (modalBtn && modalOverlay && closeBtn) {
+  modalBtn.addEventListener("click", () => {
+    modalOverlay.classList.add("open-modal");
+  });
+  closeBtn.addEventListener("click", () => {
     modalOverlay.classList.remove("open-modal");
-  }
-});
+  });
+
+  // ! 추가기능
+  // 배경 클릭시 모달 닫기
+  modalOverlay.addEventListener("click", () => {
+    modalOverlay.classList.remove("open-modal");
+  });
+  // 닫기 버튼에서 포커스 벗어나는 것 방지
+  closeBtn.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (e.key === "Tab") {
+      e.preventDefault();
+    }
+  });
+  // esc 입력시 모달 닫기
+  document.body.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (modalOverlay.classList.contains("open-modal") && e.key === "Escape") {
+      modalOverlay.classList.remove("open-modal");
+    }
+  });
+}
