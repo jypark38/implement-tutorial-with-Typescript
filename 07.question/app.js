baeb1036d7c24a775022228d0b4183cbdc42e8c3
jypark38 <jury124@naver.com>
2024-01-25 19:20:11 +0900

    refactor : type 명시 + type guard

--- 07.question/app.js
+++ 07.question/app.js
@@ -1,29 +1,30 @@
 // using selectors inside the element
 // traversing the dom
 // 방법 1
-// const btns = document.querySelectorAll(".question-btn");
-// btns.forEach((button) => {
-//   button.addEventListener("click", (event) => {
-//     const question = (event.currentTarget as HTMLButtonElement).closest(".question");
-//     const currentOpen = document.querySelector(".show-text");
-//     if (currentOpen) {
-//       currentOpen.classList.remove("show-text");
+const btns = document.querySelectorAll(".question-btn");
+btns.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const question = event.currentTarget.closest(".question");
+        const currentOpen = document.querySelector(".show-text");
+        if (currentOpen) {
+            currentOpen.classList.remove("show-text");
+        }
+        if (question) {
+            question.classList.toggle("show-text");
+        }
+    });
+});
+export {};
+// 답안
+// const questions = document.querySelectorAll(".question");
+// questions.forEach((question) => {
+//   const btn = question.querySelector(".question-btn");
+//   questions.forEach((item) => {
+//     if (item !== question) {
+//       item.classList.remove("show-text");
 //     }
+//   });
+//   btn.addEventListener("click", () => {
 //     question.classList.toggle("show-text");
 //   });
 // });
-// 답안
-const questions = document.querySelectorAll(".question");
-
-questions.forEach((question) => {
-  const btn = question.querySelector(".question-btn");
-  btn.addEventListener("click", () => {
-    questions.forEach((item) => {
-      if (item !== question) {
-        item.classList.remove("show-text");
-      }
-    });
-    question.classList.toggle("show-text");
-  });
-});
-export {};
--- 07.question/app.ts
+++ 07.question/app.ts
@@ -2,34 +2,35 @@
 // traversing the dom
 
 // 방법 1
-// const btns = document.querySelectorAll(".question-btn");
+const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".question-btn");
 
-// btns.forEach((button) => {
-//   button.addEventListener("click", (event) => {
-//     const question = (event.currentTarget as HTMLButtonElement).closest(".question");
+btns.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const question: HTMLElement | null = (event.currentTarget as HTMLButtonElement).closest(".question");
 
-//     const currentOpen = document.querySelector(".show-text");
-//     if (currentOpen) {
-//       currentOpen.classList.remove("show-text");
+    const currentOpen: HTMLElement | null = document.querySelector(".show-text");
+    if (currentOpen) {
+      currentOpen.classList.remove("show-text");
+    }
+    if (question) {
+      question.classList.toggle("show-text");
+    }
+  });
+});
+
+// 답안
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach((question) => {
+//   const btn = question.querySelector(".question-btn");
+
+//   questions.forEach((item) => {
+//     if (item !== question) {
+//       item.classList.remove("show-text");
 //     }
+//   });
 
+//   btn.addEventListener("click", () => {
 //     question.classList.toggle("show-text");
 //   });
 // });
-
-// 답안
-const questions = document.querySelectorAll(".question");
-
-questions.forEach((question) => {
-  const btn = question.querySelector(".question-btn");
-
-  questions.forEach((item) => {
-    if (item !== question) {
-      item.classList.remove("show-text");
-    }
-  });
-
-  btn.addEventListener("click", () => {
-    question.classList.toggle("show-text");
-  });
-});
