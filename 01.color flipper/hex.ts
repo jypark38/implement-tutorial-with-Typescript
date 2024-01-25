aea29e60a6d5ef0d6749c98c1d5463f321ebb821
jypark38 <jury124@naver.com>
2024-01-25 17:29:44 +0900

    01, typescript migration

--- 01.color flipper/hex.ts
+++ 01.color flipper/hex.ts
@@ -1,19 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
 btn.addEventListener("click", () => {
-  const HEX = getRandomHex();
+  const HEX: string = getRandomHex();
+
   document.body.style.backgroundColor = HEX;
   color.textContent = HEX;
 });
-const getRandomNumber = () => {
+
+const getRandomNumber = (): number => {
   return Math.floor(Math.random() * hex.length);
 };
-const getRandomHex = () => {
+
+const getRandomHex = (): string => {
   let hcolor = "#";
   for (let i = 0; i < 6; i++) {
     hcolor += hex[getRandomNumber()];
   }
   return hcolor;
 };
-export {};
