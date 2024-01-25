02675a875b51f7f0808094f9958aa206628033d6
jypark38 <jury124@naver.com>
2024-01-25 19:16:44 +0900

    07 question

--- 07.question/app.js
+++ 07.question/app.js
@@ -1,3 +1,29 @@
-export {};
-//using selectors inside the element
+// using selectors inside the element
 // traversing the dom
+// 방법 1
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     const question = (event.currentTarget as HTMLButtonElement).closest(".question");
+//     const currentOpen = document.querySelector(".show-text");
+//     if (currentOpen) {
+//       currentOpen.classList.remove("show-text");
+//     }
+//     question.classList.toggle("show-text");
+//   });
+// });
+// 답안
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+export {};
