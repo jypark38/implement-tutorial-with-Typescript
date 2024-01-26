2169b68aa24a5a83d7e682a9fbc8afe3d1cb2ea7
jypark38 <jury124@naver.com>
2024-01-26 16:02:24 +0900

    type guard

--- 09.video/app.js
+++ 09.video/app.js
@@ -3,15 +3,17 @@
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
-btn.addEventListener("click", () => {
-    if (!btn.classList.contains("slide")) {
-        btn.classList.add("slide");
-        video.pause();
-    }
-    else {
-        btn.classList.remove("slide");
-        video.play();
-    }
+window.addEventListener("DOMContentLoaded", () => {
+    btn.addEventListener("click", () => {
+        if (!btn.classList.contains("slide")) {
+            btn.classList.add("slide");
+            video.pause();
+        }
+        else {
+            btn.classList.remove("slide");
+            video.play();
+        }
+    });
 });
 // preloader
 const preloader = document.querySelector(".preloader");
--- 09.video/app.ts
+++ 09.video/app.ts
@@ -5,18 +5,20 @@
 const btn: HTMLButtonElement = document.querySelector(".switch-btn");
 const video: HTMLVideoElement = document.querySelector(".video-container");
 
-btn.addEventListener("click", () => {
-  if (!btn.classList.contains("slide")) {
-    btn.classList.add("slide");
-    video.pause();
-  } else {
-    btn.classList.remove("slide");
-    video.play();
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  btn.addEventListener("click", () => {
+    if (!btn.classList.contains("slide")) {
+      btn.classList.add("slide");
+      video.pause();
+    } else {
+      btn.classList.remove("slide");
+      video.play();
+    }
+  });
 });
 
 // preloader
-const preloader = document.querySelector(".preloader");
+const preloader: HTMLDivElement = document.querySelector(".preloader");
 
 // load : DOM 생성을 포함해서 이미지,css,js 파일이 로드됐는지 체크한다.
 // mdn - load 이벤트는 리소스와 그것에 의존하는 리소스들의 로딩이 완료되면 실행됩니다
