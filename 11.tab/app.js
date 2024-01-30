da584821994eab899815e70d1b29b7d69adb195d
jypark38 <jury124@naver.com>
2024-01-30 19:15:22 +0900

    이벤트 위임

--- 11.tab/app.js
+++ 11.tab/app.js
@@ -1,17 +1,32 @@
-const btns = document.querySelectorAll(".tab-btn");
-const articles = document.querySelectorAll(".content");
-btns.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        const activeBtn = event.currentTarget;
-        const id = activeBtn.dataset.id;
-        btns.forEach((button) => {
-            button.classList.remove("active");
-        });
-        articles.forEach((article) => {
-            article.classList.remove("active");
-        });
-        activeBtn.classList.add("active");
+// const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".tab-btn");
+// const articles: NodeListOf<HTMLDivElement> = document.querySelectorAll(".content");
+// btns.forEach((button) => {
+//   button.addEventListener("click", (event: MouseEvent) => {
+//     const activeBtn = event.currentTarget as HTMLButtonElement;
+//     const id = activeBtn.dataset.id;
+//     btns.forEach((button) => {
+//       button.classList.remove("active");
+//     });
+//     articles.forEach((article) => {
+//       article.classList.remove("active");
+//     });
+//     activeBtn.classList.add("active");
+//     document.getElementById(id).classList.add("active");
+//   });
+// });
+/// 이벤트 위임
+const btnContainer = document.querySelector(".btn-container");
+const aboutContainer = document.querySelector(".about-content");
+btnContainer.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.tagName == "BUTTON") {
+        const id = target.dataset.id;
+        const currentActiveTab = btnContainer.querySelector(".active");
+        const currentActiveAbout = aboutContainer.querySelector(".active");
+        currentActiveTab.classList.remove("active");
+        currentActiveAbout.classList.remove("active");
+        target.classList.add("active");
         document.getElementById(id).classList.add("active");
-    });
+    }
 });
 export {};
--- 11.tab/app.ts
+++ 11.tab/app.ts
@@ -1,19 +1,39 @@
-const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".tab-btn");
-const articles: NodeListOf<HTMLDivElement> = document.querySelectorAll(".content");
+// const btns: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".tab-btn");
+// const articles: NodeListOf<HTMLDivElement> = document.querySelectorAll(".content");
 
-btns.forEach((button) => {
-  button.addEventListener("click", (event: MouseEvent) => {
-    const activeBtn = event.currentTarget as HTMLButtonElement;
-    const id = activeBtn.dataset.id;
+// btns.forEach((button) => {
+//   button.addEventListener("click", (event: MouseEvent) => {
+//     const activeBtn = event.currentTarget as HTMLButtonElement;
+//     const id = activeBtn.dataset.id;
 
-    btns.forEach((button) => {
-      button.classList.remove("active");
-    });
-    articles.forEach((article) => {
-      article.classList.remove("active");
-    });
+//     btns.forEach((button) => {
+//       button.classList.remove("active");
+//     });
+//     articles.forEach((article) => {
+//       article.classList.remove("active");
+//     });
 
-    activeBtn.classList.add("active");
+//     activeBtn.classList.add("active");
+//     document.getElementById(id).classList.add("active");
+//   });
+// });
+
+/// 이벤트 위임
+
+const btnContainer: HTMLDivElement = document.querySelector(".btn-container");
+const aboutContainer: HTMLDivElement = document.querySelector(".about-content");
+
+btnContainer.addEventListener("click", (event) => {
+  const target = event.target as HTMLElement;
+  if (target.tagName == "BUTTON") {
+    const id = target.dataset.id;
+    const currentActiveTab: HTMLButtonElement = btnContainer.querySelector(".active");
+    const currentActiveAbout: HTMLDivElement = aboutContainer.querySelector(".active");
+
+    currentActiveTab.classList.remove("active");
+    currentActiveAbout.classList.remove("active");
+
+    target.classList.add("active");
     document.getElementById(id).classList.add("active");
-  });
+  }
 });
